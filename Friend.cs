3210059dab622243917ca8d8a98740c83c299f23
PAWEŁ PAWŁOW <pawel@pawlow.org>
2016-11-03 15:31:47 +0100

    Simple firends app.

---
+++ Friend.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FriendsLib
+{
+    public class Friend
+    {
+        private readonly List<Friend> friends = new List<Friend>();
+
+        public Friend(int id)
+        {
+            Id = id;
+        }
+
+        public void AddFriends(params Friend[] friends)
+        {
+            foreach (var f in friends)
+            {
+                if(!f.Friends.Contains(this))
+                    f.AddFriend(this);
+
+                if(!Friends.Contains(f))
+                AddFriend(f);
+            }
+        }
+
+        private void AddFriend(Friend friend)
+        {
+            friends.Add(friend);
+        }
+
+        public Friend[] GetFriends(int level)
+        {
+            if (level < 0)
+                throw new ArgumentOutOfRangeException(nameof(level));
+
+            if (level == 0)
+                return new[] { this };
+
+            if (level == 1)
+                return Friends.ToArray();
+
+            var result = new Dictionary<int, Friend[]>() {
+                { 0, new[] { this } },
+                { 1, Friends.ToArray()}
+            };
+
+            Func<Friend, bool> IsNotInResultYet = (f => result.Values.All(list => !list.Contains(f)));
+
+            for (int i = 2; i <= level; i++)
+            {
+                var levelFriends = new List<Friend>();
+                foreach (var friend in result[i - 1])
+                {
+                    var friendFriends = friend.GetFriends(1).Where(IsNotInResultYet);
+                    levelFriends.AddRange(friendFriends);
+                }
+                result[i] = levelFriends.Distinct().ToArray();
+            }
+
+            return result[level];
+        }
+
+        public int Id { get; set; }
+
+        public override string ToString()
+        {
+            return $"F{Id}";
+        }
+
+
+        public IEnumerable<Friend> Friends => friends;
+    }
+}
---
+++ FriendTests.cs
@@ -0,0 +1,82 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Linq;
+
+namespace FriendsLib.Tests
+{
+    [TestClass()]
+    public class FriendTests
+    {
+        static Friend[] Data = MakeFriends();
+
+        static Friend[] MakeFriends()
+        {
+            var f = Enumerable.Range(0, 15).Select(id => new Friend(id)).ToArray();
+            f[1].AddFriends(f[2]);
+            f[2].AddFriends(f[3], f[6]);
+            f[3].AddFriends(f[4], f[7]);
+            f[4].AddFriends(f[5], f[8]);
+            f[6].AddFriends(f[10]);
+            f[7].AddFriends(f[8], f[11]);
+            f[8].AddFriends(f[11]);
+            f[9].AddFriends(f[11], f[13], f[14]);
+            f[11].AddFriends(f[12]);
+            return f;
+        }        
+
+        [TestMethod()]
+        public void GetFriends0Test()
+        {
+            var f8 = Data[8];
+            var friends0 = f8.GetFriends(0);
+            CollectionAssert.AreEquivalent(new[] { Data[8] }, friends0);
+        }
+
+        [TestMethod()]
+        public void GetFriends1Test()
+        {
+            var f8 = Data[8];
+            var friends1 = f8.GetFriends(1);
+            CollectionAssert.AreEquivalent(new[] { Data[4], Data[7], Data[11] }, friends1);            
+        }
+
+        [TestMethod()]
+        public void GetFriends2Test()
+        {
+            var f8 = Data[8];
+            var friends2 = f8.GetFriends(2);
+            CollectionAssert.AreEquivalent(new[] { Data[3], Data[5], Data[9], Data[12] }, friends2);            
+        }
+
+        [TestMethod()]
+        public void GetFriends3Test()
+        {
+            var f8 = Data[8];            
+            var friends3 = f8.GetFriends(3);
+            CollectionAssert.AreEquivalent(new[] { Data[2], Data[13], Data[14] }, friends3);            
+        }
+
+        [TestMethod()]
+        public void GetFriends4Test()
+        {
+            var f8 = Data[8];            
+            var friends4 = f8.GetFriends(4);
+            CollectionAssert.AreEquivalent(new[] { Data[1], Data[6] }, friends4);            
+        }
+
+        [TestMethod()]
+        public void GetFriends5Test()
+        {
+            var f8 = Data[8];            
+            var friends5 = f8.GetFriends(5);
+            CollectionAssert.AreEquivalent(new[] { Data[10] }, friends5);
+        }
+
+        [TestMethod()]
+        public void GetFriends6Test()
+        {
+            var f8 = Data[8];
+            var friends6 = f8.GetFriends(6);
+            CollectionAssert.AreEquivalent(new Friend[0], friends6);
+        }
+    }
+}
